f81b25c47e5199cbd490b0a05f9acc1b49bb6ea7
stsiapan <55788184+stsiapan@users.noreply.github.com>
2020-01-14 17:18:43 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,18 +1,26 @@
 import psutil
 
+alarm_percent = 90
+alarm_var = psutil.disk_usage('/')
 ps_list = psutil.pids()
 
+if alarm_var.percent > alarm_percent:
+    print ('warning! free space will end soon!!!')
+else:
+    print ('you have a lot of free space')
+
 for i in ps_list:
-    process =  psutil.Process(i)    
+    process =  psutil.Process(i)
     if process.name() == 'java':
         a = process.name()
         b = process.pid
 
 try:
     if a:
-        print ('java process is running and have pid: {0} '.format (b))
+        print ('java process is running and have pid: {} '.format (b))
 except:
     print ('java is over')
 
-print ("memory usage: ",  psutil.virtual_memory())
-print ("disk usage: ", psutil.disk_usage('/'))
+print ('memory usage: ', psutil.virtual_memory())
+print ('disk usage: {}'.format (alarm_var))
+
