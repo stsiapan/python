928846905a818dbed8dff1764e34261d26509908
stsiapan <55788184+stsiapan@users.noreply.github.com>
2020-01-15 17:35:39 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,27 +4,26 @@ alarm_percent = 90
 alarm_var = psutil.disk_usage('/')
 ps_list = psutil.pids()
 memory_usage = psutil.virtual_memory()
+human_read = 1024000000.
 
 disk_u = {
-    'total disk usage (Gb):': alarm_var.total/1024000000.,
-    'used disk usage (Gb):': alarm_var.used/1024000000.,
+    'total disk usage (Gb):': alarm_var.total/human_read,
+    'used disk usage (Gb):': alarm_var.used/human_read,
     'percent disk usage (%):': alarm_var.percent
 }
 
 mem_u = {
-    'total memory (Gb):': memory_usage.total/1024000000.,
-    'available memory (Gb):': memory_usage.total/1024000000.,
-    'used memory (Gb):': memory_usage.used/1024000000.,
-    'free memory (Gb):': memory_usage.free/1024000000.,
+    'total memory (Gb):': memory_usage.total/human_read,
+    'available memory (Gb):': memory_usage.total/human_read,
+    'used memory (Gb):': memory_usage.used/human_read,
+    'free memory (Gb):': memory_usage.free/human_read,
     'percent (%):': memory_usage.percent
 }
 
 if alarm_var.percent > alarm_percent:
-    #print ('warning! free space will end soon!!!')
     with open ('my.html', 'w') as f:
         f.write('<h1 style="color:red;">warning! free space will end soon!!!</h1>\n')
 else:
-    #print ('you have a lot of free space')
     with open ('my.html', 'w') as f:
         f.write('<h1 style="color:green;">you have a lot of free space</h1>\n')
 
@@ -36,11 +35,9 @@ for i in ps_list:
 
 try:
     if a:
-        #print ('java process is running and have pid: {} '.format (b))
         with open ('my.html', 'a') as f:
             f.write('<h2 style="color:green;">java process is running and have pid: {}</h2>\n'.format (b))
 except:
-    #print ('<h2 style="color:red;">java is over</h2>\n')
     with open ('my.html', 'a') as f:
         f.write('<h2 style="color:red;">java is over</h2>\n')
 
