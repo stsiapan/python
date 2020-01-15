943fa661ca903eef6f1502679b010d45d0967b57
stsiapan <55788184+stsiapan@users.noreply.github.com>
2020-01-15 14:34:06 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,7 +4,21 @@ alarm_percent = 90
 alarm_var = psutil.disk_usage('/')
 ps_list = psutil.pids()
 memory_usage = psutil.virtual_memory()
+delimeter = "####****####"
 
+disk_u = {
+    'total disk usage (Gb):': alarm_var.total/1024000000.,
+    'used disk usage (Gb):': alarm_var.used/1024000000.,
+    'percent disk usage (%):': alarm_var.percent
+}
+
+mem_u = {
+    'total memory (Gb):': memory_usage.total/1024000000.,
+    'available memory (Gb):': memory_usage.total/1024000000.,
+    'used memory (Gb):': memory_usage.used/1024000000.,
+    'free memory (Gb):': memory_usage.free/1024000000.,
+    'percent (%):': memory_usage.percent
+}
 
 if alarm_var.percent > alarm_percent:
     #print ('warning! free space will end soon!!!')
@@ -31,5 +45,14 @@ except:
     with open ('my.html', 'a') as f:
         f.write('<h2 style="color:red;">java is over</h2>\n')
 
-with open ('my.html', 'a') as f:
-    f.write('###***###\n<h3 style="color:blue;">memory is: {}</h3>\n###***###\n<h3 style="color:blue;">disk usage is: {}</h3>\n'.format(alarm_var, memory_usage))
+for i in disk_u:
+    #print i, disk_u[i]
+    with open ('my.html', 'a') as f:
+        for i in disk_u:
+            f.write('<h3 style="color:brown;">{} {}</h3>\n'.format(i, disk_u[i]))
+
+for i in mem_u:
+    # print i, mem_u[i]
+    with open ('my.html', 'a') as f:
+        for i in mem_u:
+            f.write('<h3 style="color:blue;">{} {}</h3>\n'.format(i, mem_u[i]))
